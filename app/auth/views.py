6976cc3795c0a5206abfe2058f5b9cc665c85893
Anthony Pernia <anthonyperniah@gmail.com>
2021-11-03 12:12:16 +0000

    finalizado, falta la parte visual

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,18 +1,78 @@
+from flask_login.utils import login_user, logout_user, login_required
+from flask_login import current_user
+from app.model import UserData, UserModel
 from . import auth
 from app.forms import LoginForm
 from flask import render_template, flash, url_for, redirect, session
+from app.firestore_service import get_user, user_put
+from werkzeug.security import generate_password_hash, check_password_hash
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
     context = {
         'login_form': LoginForm()
     }
     login_form = LoginForm()
     if login_form.validate_on_submit():
         username = login_form.username.data
-        session['username'] = username
+        password = login_form.password.data
 
-        flash('registrado con exito {}'.format(username))
+        user_doc = get_user(username)
 
+        if user_doc.to_dict() is not None:
+            password_from_db = user_doc.to_dict()['password']
+
+            if check_password_hash(password_from_db, password):
+                user_data = UserData(username, password)
+                user = UserModel(user_data)
+                login_user(user)
+                flash('Bienvenido de nuevo')
+                redirect(url_for('hello'))
+
+            else:
+                flash('Usuario o contraseña incorrectos')
+                
+        else:
+            flash('Usuario no existe')
+         
         return redirect(url_for('index'))
     return render_template('login.html', **context)
+
+@auth.route('logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Sesión cerrada')
+    return redirect(url_for('auth.login'))
+
+@auth.route('signup', methods = ['GET', 'POST'])
+def singup():
+    signup_form = LoginForm()
+    context = {
+        'signup_form': signup_form
+    }
+    if signup_form.validate_on_submit():
+        username = signup_form.username.data
+        password = signup_form.password.data
+    
+        user_doc = get_user(username)
+    
+        if user_doc.to_dict() is None:
+            password_hash = generate_password_hash(password)
+            user_data = UserData(username, password_hash)
+            user_put(user_data)
+            user = UserModel(user_data)
+
+            login_user(user)
+
+            flash('Bienvenido')
+            return redirect(url_for('hello'))
+        else:
+            flash('El usuario ya existe')
+           
+        
+    return render_template('signup.html', **context)
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from flask import  request, render_template, redirect, make_response, session, url_for , flash
-from flask_bootstrap import Bootstrap
-from app.forms import LoginForm
 import unittest
 from app import create_app
-from app.firestore_service import get_users, get_todos
+from flask_login import login_required, current_user
+from app.firestore_service import delete_todo, get_users, get_todos, put_todo, update_todo
+from app.forms import TodoForm, DeleteTodoForm, UpdateTodoForm
 
 app = create_app()
 
@@ -34,21 +34,42 @@ def index():
     session['user_ip'] =  user_ip
     return response
 
-@app.route('/hello', methods=['GET'])
+@app.route('/hello', methods=['GET', 'POST'])
+@login_required
 def hello():
+
     user_ip = session.get('user_ip')
-    username = session.get('username')
+    username = current_user.id
+
+    todo_form = TodoForm()
+    delete_form = DeleteTodoForm()
+    update_form = UpdateTodoForm()
+
     context = {
         'user_ip':user_ip, 
         'todos':get_todos(user_id=username),
-        'username' : username
+        'username' : username,
+        'todo_form' : todo_form,
+        'delete_form' : delete_form,
+        'update_form' : update_form
+
     }
 
-    users = get_users()
-    for user in users:
-        print(user.id)
-        print(user.to_dict()['password'])
-    
-    
+    if todo_form.validate_on_submit():
+        put_todo(user_id=username, description=todo_form.description.data)
+        flash('Tarea creada con exito')
+        return redirect(url_for('hello'))
 
     return render_template('hello.html', **context)
+
+@app.route('/todos/delete/<todo_id>', methods=['POST'])
+def delete(todo_id):
+    user_id = current_user.id
+    delete_todo(user_id, todo_id)
+    return redirect(url_for('hello'))
+
+@app.route('/todos/update/<todo_id>/<int:done>', methods=['POST'])
+def update(todo_id, done):
+    user_id = current_user.id
+    update_todo(user_id, todo_id, done)
+    return redirect(url_for('hello'))
